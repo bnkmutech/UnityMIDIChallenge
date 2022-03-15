99f4428c5bfdc651a97b473d4fddfb17cdb306b4
jukrapopk <jukrapopk@gmail.com>
2022-03-16 00:24:57 +0700

    Create struct and scriptable object to manage notes

---
+++ Assets/Scripts/ScriptableObjectTemplates/NoteSet.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace ScriptableObjectTemplates
+{
+    [CreateAssetMenu(fileName = "NoteScriptableObject", menuName = "ScriptableObjects/NoteSet")]
+    public class NoteSet : ScriptableObject
+    {
+        public Note[] notes;
+    }
+}
